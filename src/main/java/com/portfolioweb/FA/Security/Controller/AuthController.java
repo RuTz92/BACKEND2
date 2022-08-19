3e1ea9f5ed1a4319d6f8e163623faaf9cd6807de
RuTz92 <franco.angiolini@gmail.com>
2022-08-19 14:39:11 -0300

    Update AuthController.java

--- src/main/java/com/portfolioweb/FA/Security/Controller/AuthController.java
+++ src/main/java/com/portfolioweb/FA/Security/Controller/AuthController.java
@@ -31,7 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://francoangiolinipw.firebaseapp.com")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;

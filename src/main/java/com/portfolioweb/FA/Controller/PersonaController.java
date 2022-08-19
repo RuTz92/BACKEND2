af3372e81ec6f23db98e34c8897b6347615672eb
RuTz92 <franco.angiolini@gmail.com>
2022-08-19 14:20:58 -0300

    Conexion con Front

--- src/main/java/com/portfolioweb/FA/Controller/PersonaController.java
+++ src/main/java/com/portfolioweb/FA/Controller/PersonaController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://francoangiolinipw.firebaseapp.com/")
 public class PersonaController {
     @Autowired 
     IPersonaService ipersonaService;

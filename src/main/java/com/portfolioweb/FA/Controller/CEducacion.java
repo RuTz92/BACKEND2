8ea905ea0c2c72f86b5932b2467ff4d1ace38cd9
RuTz92 <franco.angiolini@gmail.com>
2022-10-08 07:35:32 -0300

    Volviendo a local host. Elimino Skills

--- src/main/java/com/portfolioweb/FA/Controller/CEducacion.java
+++ src/main/java/com/portfolioweb/FA/Controller/CEducacion.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/educacion")
 @CrossOrigin(origins = "https://francoangiolinipw.web.app")
+//@CrossOrigin(origins = "http://localhost:4200")
 public class CEducacion {
     
     @Autowired
--- src/main/java/com/portfolioweb/FA/Controller/CExperiencia.java
+++ src/main/java/com/portfolioweb/FA/Controller/CExperiencia.java
@@ -23,6 +23,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/explab")
 @CrossOrigin(origins = "https://francoangiolinipw.web.app")
+//@CrossOrigin(origins = "http://localhost:4200")
+
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;

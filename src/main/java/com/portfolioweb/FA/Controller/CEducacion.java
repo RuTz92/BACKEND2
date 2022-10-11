8828c6e499003f74c8b1bdb9e073e944727369cd
RuTz92 <franco.angiolini@gmail.com>
2022-10-11 10:21:45 -0300

    Revert "Probando causa de error en Heroku"
    This reverts commit 29af5ba014297111cd8a9e501571970a372d9361.

--- src/main/java/com/portfolioweb/FA/Controller/CEducacion.java
+++ src/main/java/com/portfolioweb/FA/Controller/CEducacion.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/educacion")
 
-@CrossOrigin(origins = "https://francoangiolinipw.web.app")
+@CrossOrigin(origins = {"http://localhost:4200","https://francoangiolinipw.web.app"})
 public class CEducacion {
     
     @Autowired
--- src/main/java/com/portfolioweb/FA/Controller/CExperiencia.java
+++ src/main/java/com/portfolioweb/FA/Controller/CExperiencia.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = "https://francoangiolinipw.web.app")
+@CrossOrigin(origins = {"http://localhost:4200","https://francoangiolinipw.web.app"})
 
 public class CExperiencia {
     @Autowired
